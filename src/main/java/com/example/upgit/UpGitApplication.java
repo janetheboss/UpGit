1f1aec4d36d1d8e76afdd7f0e6158883203c04a3
janetheboss <101011386+janetheboss@users.noreply.github.com>
2023-03-06 14:46:36 +0200

    Yancho was here

---
+++ src/main/java/com/example/upgit/UpGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.upgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpGitApplication {
+    public static void main(String[] args) {
+        System.out.println("Up Project");
+        SpringApplication.run(UpGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/upgit/UpGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
